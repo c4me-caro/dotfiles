5faf679bba976c158c3be8c39744c5d908182b67
caleb1279 <caleb.dcaro@colnexsi.com.co>
2023-08-26 13:28:39 -0500

    first commit

---
+++ spaceship/spaceship.zsh
@@ -0,0 +1,31 @@
+# Display time
+SPACESHIP_TIME_SHOW=false
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# Show directorys
+SPACESHIP_DIR_SHOW=true
+
+# Add virtualenv generic commands
+SPACESHIP_VENV_GENERIC_NAMES=(virtualenv venv .virtualenv .venv pipenv .pipenv .env env)
+
+# Show exit code
+SPACESHIP_EXIT_CODE_SHOW=true
+
+# Cut path to ../path1/path2/path3/
+SPACESHIP_DIR_TRUNC=2
+SPACESHIP_DIR_TRUNC_PREFIX="../"
+
+# Show time of execution of last command
+SPACESHIP_EXEC_TIME_SHOW=true
+SPACESHIP_EXEC_TIME_ELAPSED=2
+SPACESHIP_EXEC_TIME_PRECISION=2
+
+# Git
+SPACESHIP_GIT_BRANCH_SHOW=true
+SPACESHIP_GIT_STATUS_SHOW=true
+
